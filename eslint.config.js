5a6ede7458555cdb2f29cfae4549f5fa0b3e7c2b
Vitor Fogaça Soares <107561198+Vitorfogaca10@users.noreply.github.com>
2025-08-25 13:44:02 -0300

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+error during build:
+[vite:build-html] Failed to resolve /src/main.jsx from /vercel/path0/index.html
+file: /vercel/path0/index.html
+    at file:///vercel/path0/node_modules/vite/dist/node/chunks/dep-Bj7gA1-0.js:26765:64
+    at async Promise.all (index 0)
+    at async Object.handler (file:///vercel/path0/node_modules/vite/dist/node/chunks/dep-Bj7gA1-0.js:26866:5)
+    at async transform (file:///vercel/path0/node_modules/rollup/dist/es/shared/node-entry.js:21108:16)
+    at async ModuleLoader.addModuleSource (file:///vercel/path0/node_modules/rollup/dist/es/shared/node-entry.js:21321:36)
+Error: Command "npm run build" exited with 1
