f100d52ec0b82e1da36047d20c4b459ffc444fb9
Vitor Fogaça Soares <107561198+Vitorfogaca10@users.noreply.github.com>
2025-08-25 14:40:30 -0300

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -14,17 +14,7 @@ function Home() {
         <div className="navbar">
             <header className="container">
                 <div className="logo">BRAPY</div>
-
-                {/* Botão hamburguer visível só no mobile */}
-                <button 
-                  className="hamburger" 
-                  onClick={() => setMenuOpen(!menuOpen)}
-                >
-                    ☰
-                </button>
-
-                {/* Nav com toggle */}
-                <nav className={`link-btt ${menuOpen ? "open" : ""}`}>
+                <nav className="link-btt">
                     <ul className="links">
                         <a href="#">Home</a>
                         <a href="#comofunciona">Como Funciona</a>
@@ -32,7 +22,7 @@ function Home() {
                         <a href="#sobre">Contato</a>
                     </ul>
                     <ul className="nav-login">
-                        <li><NavLink className="btn-login" to="/login">Login</NavLink></li>
+                        <li><NavLink className="btn-login"to="/login">Login</NavLink></li>
                     </ul>
                     <ul className="nav-cadastro ">
                         <li><button to="/" className="btn-cadastro">Cadastre-se</button></li>
@@ -129,6 +119,3 @@ function Home() {
         </div>
     )
 }
-
-
-export default Home;
